828562a40b332bbf990f6ddb1854ccd3ad8b591f
Ronald <ronald.butron@gmail.com>
2020-01-23 19:24:13 +0000

    Added log4j2 configuration

--- src/test/groovy/com/auto/steps/MyStepdefs.java
+++ src/test/groovy/com/auto/steps/MyStepdefs.java
@@ -1,12 +1,22 @@
 package com.auto.steps;
 
-import cucumber.api.java8.En;
+import io.cucumber.java8.En;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.core.Logger;
 
 public class MyStepdefs implements En {
+
+    private final static Logger logger = LogManager.getLogger(MyStepdefs.class);
 
     public MyStepdefs() {
         Given("^I print something \"([^\"]*)\"$", (String arg0) -> {
             System.out.println(arg0);
+            System.out.println(logger.isInfoEnabled());
+
+            logger.info("info! {}");
+            logger.error("error! {}");
+            logger.debug("debug! {}");
         });
     }
 }
